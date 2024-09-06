e7adfb922368e3ce1f0266a1cf8961242178ec18
FatihG34 <fthgnydn34@gmail.com>
2024-09-06 12:16:22 +0300

    initial commit

---
+++ src/main/java/com/starttohard/toHard/ToHardApplication.java
@@ -0,0 +1,13 @@
+package com.starttohard.toHard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToHardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToHardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starttohard/toHard/ToHardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starttohard.toHard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToHardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
